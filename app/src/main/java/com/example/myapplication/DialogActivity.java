c33348a4bc3c41d20301185dee8d73eec5c85996
Riddh Modi <riddhmodi2003@gmail.com>
2024-03-06 10:17:12 +0530

    Dialog Types work are completed.

--- app/src/main/java/com/example/myapplication/DialogActivity.java
+++ app/src/main/java/com/example/myapplication/DialogActivity.java
@@ -1,10 +1,14 @@
 package com.example.myapplication;
 
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -13,20 +17,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Calendar;
+
 public class DialogActivity extends AppCompatActivity {
-    Button btn1,btn2;
+    Button btn1,btn2,btn3,btn4;
+    Calendar c = Calendar.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_dialog);
         btn1 = findViewById(R.id.button1);
         btn2 = findViewById(R.id.button2);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        btn3 = findViewById(R.id.button3);
+        btn4 = findViewById(R.id.button4);
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -41,10 +44,48 @@ public class DialogActivity extends AppCompatActivity {
                         }).setNegativeButton("NO", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-
+                                dialog.dismiss();
                             }
                         }).show();
             }
         });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(DialogActivity.this)
+                        .setTitle("Payment Done")
+                        .setMessage("Sucess")
+                        .setNeutralButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(DialogActivity.this, "OK", Toast.LENGTH_SHORT).show();
+                            }
+                        }).show();
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(DialogActivity.this,SETDATE,c.get(Calendar.YEAR),c.get(Calendar.MONTH),c.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new TimePickerDialog(DialogActivity.this,SETTIME,c.get(Calendar.HOUR),c.get(Calendar.MINUTE),false).show();
+            }
+        });
     }
+    private DatePickerDialog.OnDateSetListener SETDATE = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            btn3.setText(dayOfMonth+"/"+(month+1)+"/"+year);
+        }
+    };
+    private TimePickerDialog.OnTimeSetListener SETTIME = new TimePickerDialog.OnTimeSetListener() {
+        @Override
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            btn4.setText(hourOfDay+":"+minute);
+        }
+    };
 }
